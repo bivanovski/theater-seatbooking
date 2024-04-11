1799252d8a2fd8e38f376d007c74170a72272480
BoIv1412 <bojan.ivanovski@studentambassadors.com>
2024-04-11 11:41:33 +0200

    database folder added

---
+++ Database/Connection.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database;
+
+class Connection
+{
+    const HOST = 'localhost';
+    const DB_NAME = 'theatreDB';
+    const USERNAME = 'root';
+    const PASSWORD = '';
+
+    protected $connection;
+
+    public function __construct()
+    {
+        $this->connection = new \PDO('mysql:host=' . self::HOST . ';dbname=' . self::DB_NAME, self::USERNAME, self::PASSWORD);
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    public function destroy()
+    {
+        $this->connection = null;
+    }
+}
+
+?>
