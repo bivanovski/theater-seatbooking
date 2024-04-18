54f02bb6286e2e295980b4b10b168fda371baf85
akrstevska <krstevski@digimak.com.mk>
2024-04-18 15:08:08 +0200

    created Reservation class

---
+++ Reservations/Reservation.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Reservations;
+
+require_once (__DIR__ . '/../Database/Connection.php');
+
+use Database\Connection as Connection;
+
+class Reservation
+{
+    protected $id;
+    protected $user_id;
+    protected $row;
+    protected $seat_num;
+    protected $repertoire_id;
+    protected $seat_type_id;
+    protected $is_confirmed;
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    public function setId($id)
+    {
+        $this->id = $id;
+
+    }
+
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+
+    }
+
+    public function getRow()
+    {
+        return $this->row;
+    }
+
+
+    public function setRow($row)
+    {
+        $this->row = $row;
+
+    }
+
+    public function getSeatNum()
+    {
+        return $this->seat_num;
+    }
+
+    function setSeatNum($seat_num)
+    {
+        $this->seat_num = $seat_num;
+
+    }
+
+
+    public function getRepertoireId()
+    {
+        return $this->repertoire_id;
+    }
+
+
+    public function setRepertoireId($repertoire_id)
+    {
+        $this->repertoire_id = $repertoire_id;
+
+    }
+
+    public function getSeatTypeId()
+    {
+        return $this->seat_type_id;
+    }
+
+    public function setSeatTypeId($seat_type_id)
+    {
+        $this->seat_type_id = $seat_type_id;
+
+    }
+
+
+    public function getIsConfirmed()
+    {
+        return $this->is_confirmed;
+    }
+
+    public function setIsConfirmed($is_confirmed)
+    {
+        $this->is_confirmed = $is_confirmed;
+
+    }
+
+}
